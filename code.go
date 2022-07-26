1e7d9e8c966cbb57185edc3f5f33dd69ecb78202
S3CUR1TY <30468513+S-3-C-U-R-1-T-Y@users.noreply.github.com>
2022-07-26 08:10:02 +0200

    Update code.go

--- code.go
+++ code.go
@@ -1,4 +1,4 @@
 "AadClientId": " ff63caf9-327c-4eaf-a989-dd3c1c875a55",
-"AadSecret": " OUz8Q~Sj7o7pthDCvJjLfMQ7mtH-xe6Tmrycobwm ",
+"AadSecret": " OUz8Q~Sj7o7pthDCvJjLfMQ7mtH-xe6Tmrycobwm",
 "AadTenantDomain": "nedscaperlab.onmicrosoft.com",
 "AadTenantId": "316bec54-8d14-4882-8af3-ee5e61570603"
